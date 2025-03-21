af728ff09e560ec61664aba0ad1c2f289f5673e7
WitiaUA <108870328+WitiaUA@users.noreply.github.com>
2025-03-21 14:53:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const bg = document.getElementById("background");
+    const markers = [
+        { id: "perdichium", x: 74.25, y: 42.99, link: "https://tsebuleve.wiki.gg/uk/wiki/Кратер_Пердичіуму" },
+        { id: "marker1", x: 30, y: 40, link: "https://uk.minecraft.wiki/" },
+        { id: "marker2", x: 60, y: 70, link: "https://uk.minecraft.wiki/" }
+    ];
 
-    bg.addEventListener("click", function(event) {
+    function updateMarkers() {
+        const container = document.querySelector(".container");
+        const bg = document.querySelector(".background");
         const rect = bg.getBoundingClientRect();
-        const x = ((event.clientX - rect.left) / rect.width) * 100;
-        const y = ((event.clientY - rect.top) / rect.height) * 100;
 
-        console.log(`Координати: x: ${x.toFixed(2)}%, y: ${y.toFixed(2)}%`);
-    });
+        markers.forEach(marker => {
+            const el = document.getElementById(marker.id);
+            el.style.left = `${rect.left + (marker.x * rect.width / 100)}px`;
+            el.style.top = `${rect.top + (marker.y * rect.height / 100)}px`;
+        });
+    }
+
+    window.addEventListener("resize", updateMarkers);
+    updateMarkers();
 });
